57fb21dc0093b62344a3250ddb5ab9792f5da549
sedatsamet <sedatoypan@gmail.com>
2023-12-14 11:16:04 +0300

    Removed gitignore files from project folder

---
+++ eureka-server/src/main/java/com/sedatsamet/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.sedatsamet.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
